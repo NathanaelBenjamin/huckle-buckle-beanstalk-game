2bdb81a2b49337baf919553a9bc94f65a6dbd971
Nathanael Benjamin <oluwakayodenathanael@gmail.com>
2022-06-11 23:16:14 +0100

    App updates: wasn't able to track previous guesses.

--- src/App.js
+++ src/App.js
@@ -2,38 +2,57 @@ import React from 'react';
 import { useState } from 'react';
 import "./App.css";
 
+let randomNumber = Math.round(Math.random()*100);
+
 const App = () => {
-  const [gameStart, setGameStart] = useState(true);
-  const [userInput, setUserInput] = useState("");
-  const [result, setResult] =useState("");
 
-  let randomNumber = Math.round(Math.random()*100);
-
+  const [gameStart, setGameStart] = useState(false);
+  const[input, setInput] = useState("");
+  const [result, setResult] = useState("");
+  
   const handleChange = (event) => {
-    setUserInput(event.target.value);
+    if(event.target.value.length > 3){alert("Guesses must not be greater than 100!"); event.target.value = event.target.value.slice(0, 3); return;} 
+    else{
+      setInput(event.target.value);
+    } 
   }
 
-  if(randomNumber === userInput){setResult("Right on! Correct guess.")}
-  else if(randomNumber > userInput){setResult("Your guess was higher than the number.")}
-  else{setResult("Your guess was lower than the number.")}
+  
+
+  
+
+  const playgame = () => {
+    console.log(randomNumber)
+    let userGuess = parseInt(input);
+
+    if(userGuess === randomNumber){
+      setResult("Right! Correct guess!");
+      setGameStart(true)
+    }
+
+    else if(userGuess > randomNumber){setResult("Oops! That was greater than the hidden number.")}
+
+    else if(userGuess < randomNumber){setResult("Oops! That was lesser than the hidden number.")}
+  }
+  
+  
 
   return (
     <div className='container'>
-      <div className="form">
-      
-        <input type="number" placeholder='Enter your guess.' 
-        onChange={handleChange} value={userInput}
-        />
-        {gameStart ? <div className='buttons'>
-          <button>Submit guess</button>
-        </div> : <button>New game</button>}
-      </div>
+      <div className="game-body">
+        <form>
+          <input type="number" placeholder='Enter your guess.' required 
+            onChange={handleChange}
+          />
+          {gameStart ? <button onClick={(event) => {event.preventDefault(); setGameStart(false)}}>Start game</button> : <button onClick={(event) => {event.preventDefault(); (input !== "") ? (input  > 100 ? playgame() : alert("Your guess should not be greater than 100.")) : alert("Error! Fill in your guess.")}}>Submit</button>}
+        </form>
 
-      <div className="result">
-        <h1>{result}</h1>
+        <div className="result">
+          {result}
+        </div>
       </div>
     </div>
   )
 }
 
-export default App;
+export default App
